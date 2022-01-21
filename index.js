9d4292618fd31f384ffc2c24c025cabe7e540762
LORD-MAIYOVA <96196848+LORD-MAIYOVA@users.noreply.github.com>
2022-01-21 22:16:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const io = require("socket.io")(8900, {
+const io = require("socket.io")("https://apisocket.herokuapp.com", {
     cors: {
         origin: "https://apifrontend---1.herokuapp.com",
     },
